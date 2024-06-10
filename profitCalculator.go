c149dc84ea5b6d697ca833641663479555ec15ba
ikem joshua <niphemijoshua@gmail.com>
2024-06-10 17:28:39 +0100

    today's update

--- profitCalculator.go
+++ profitCalculator.go
@@ -1,28 +1,44 @@
 package main
 import "fmt"
 //PROFIT CALCULATOR APP
+
 func main() {
 	fmt.Println("Welcome to the Profit Calculator app")
-	// Using revenue, expenses and tax rate
-	var revenue float64
-	var expense float64
-	var taxRate float32
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
-	fmt.Print("Enter expense: ")
-	fmt.Scan(&expense)
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
-	// Calculate earnings before tax
-	var earningsBeforeTax float64 = revenue - expense
-	fmt.Printf("Earnings before tax: $%.2f\n", earningsBeforeTax)
-	// Calculate tax
-	var tax float64 = earningsBeforeTax * float64(taxRate/100)
-	fmt.Printf("Value to be deducted(tax): $%.2f\n", tax)
-	// Calculate Profit 
-	var profit float64 = earningsBeforeTax - tax
-	fmt.Printf("Your Profit equals $%.2f\n", profit)
-	var ratio float64 = earningsBeforeTax / tax
-	fmt.Printf("Your earning ratio equals %.2f\n", ratio)
+	calculateProfit()
+}
+func outputText(text string) {
+	fmt.Print(text)
+}
 
+func calculateEarningsBeforeTax() float64 {
+		// Calculate earnings before tax
+		var revenue float64
+		var expense float64
+			// Using revenue, expenses
+		outputText("Enter revenue: ")
+		fmt.Scan(&revenue)
+		outputText("Enter expense: ")
+		fmt.Scan(&expense)
+		var earningsBeforeTax float64 = revenue - expense
+		return earningsBeforeTax
+}
+func calculateTax() float64 {
+		// Calculate tax
+		var taxRate float64	
+		outputText("Enter tax rate: ")
+		fmt.Scan(&taxRate)
+		earningsBeforeTax := calculateEarningsBeforeTax()
+		fmt.Printf("Earnings before tax: $%.2f\n", earningsBeforeTax)
+		var tax float64 = earningsBeforeTax * float64(taxRate/100)
+		return tax
+}
+func calculateProfit() {
+		// Calculate Profit
+		earningsBeforeTax := calculateEarningsBeforeTax() 
+		tax := calculateTax()
+		fmt.Printf("Value to be deducted(tax): $%.2f\n", tax)
+		var profit float64 = earningsBeforeTax - tax
+		fmt.Printf("Your Profit equals $%.2f\n", profit)
+		var ratio float64 = earningsBeforeTax / tax
+		fmt.Printf("Your earning ratio equals %.2f\n", ratio)
 }
